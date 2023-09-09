56f3e57f97fb3aa08eb9fe2dc9011aafa7797bda
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-09-09 17:46:03 +0900

    index を全部に貼ってみた

--- webapp/mysql/db/0_Schema.sql
+++ webapp/mysql/db/0_Schema.sql
@@ -37,14 +37,20 @@ CREATE TABLE isuumo.chair
     stock       INTEGER         NOT NULL
 );
 
-CREATE INDEX chair_price_id_idx ON isuumo.chair (price ASC, id ASC);
-CREATE INDEX chair_popularity_id_idx ON isuumo.chair (popularity DESC, id ASC);
+CREATE INDEX chair_price_idx ON isuumo.chair (price ASC);
 CREATE INDEX chair_height_idx ON isuumo.chair (height ASC);
 CREATE INDEX chair_width_idx ON isuumo.chair (width ASC);
-CREATE INDEX chair_color_idx ON isuumo.chair (color ASC);
 CREATE INDEX chair_depth_idx ON isuumo.chair (depth ASC);
+CREATE INDEX chair_color_idx ON isuumo.chair (color ASC);
+CREATE INDEX chair_features_idx ON isuumo.chair (features ASC);
+CREATE INDEX chair_kind_idx ON isuumo.chair (kind ASC);
+CREATE INDEX chair_popularity_idx ON isuumo.chair (popularity DESC);
+CREATE INDEX chair_stock_idx ON isuumo.chair (stock ASC);
 
-CREATE INDEX estate_popularity_id_idx ON isuumo.estate (popularity DESC, id ASC);
-CREATE INDEX estate_rent_id_idx ON isuumo.estate (rent ASC, id ASC);
+CREATE INDEX estate_address_idx ON isuumo.estate (address ASC);
+CREATE INDEX estate_point_idx ON isuumo.estate (latitude ASC, longitude ASC);
+CREATE INDEX estate_rent_idx ON isuumo.estate (rent ASC);
 CREATE INDEX estate_door_height_idx ON isuumo.estate (door_height ASC);
 CREATE INDEX estate_door_width_idx ON isuumo.estate (door_width ASC);
+CREATE INDEX estate_features_idx ON isuumo.estate (features ASC);
+CREATE INDEX estate_popularity_idx ON isuumo.estate (popularity DESC);
