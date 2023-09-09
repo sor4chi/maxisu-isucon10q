3f1550862888e7caa0ef544e4a3da9d314e99117
sor4chi <sorakawa0218@gmail.com>
2023-09-09 17:01:13 +0900

    pointをEstate構造体に追加

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -65,6 +65,7 @@ type Estate struct {
 	Description string  `db:"description" json:"description"`
 	Latitude    float64 `db:"latitude" json:"latitude"`
 	Longitude   float64 `db:"longitude" json:"longitude"`
+	Point       string  `db:"point" json:"-"`
 	Address     string  `db:"address" json:"address"`
 	Rent        int64   `db:"rent" json:"rent"`
 	DoorHeight  int64   `db:"door_height" json:"doorHeight"`
