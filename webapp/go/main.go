a49eae14ca9101adba7e87e29c2dc3f6fe543ae7
Ryoga-exe <49950206+Ryoga-exe@users.noreply.github.com>
2023-09-09 12:44:18 +0900

    fix:  を超えたら break する

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -904,6 +904,9 @@ func searchEstateNazotte(c echo.Context) error {
 			}
 		} else {
 			estatesInPolygon = append(estatesInPolygon, validatedEstate)
+			if len(estatesInPolygon) > NazotteLimit {
+				break
+			}
 		}
 	}
 
