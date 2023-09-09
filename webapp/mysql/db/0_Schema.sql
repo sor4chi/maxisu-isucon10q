787f9dbe5f75cbd9b79211a5d34fd1be01f309e2
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-09-09 12:48:20 +0900

    update schema to create index on estate (rent, id)

--- webapp/mysql/db/0_Schema.sql
+++ webapp/mysql/db/0_Schema.sql
@@ -39,3 +39,4 @@ CREATE TABLE isuumo.chair
 
 CREATE INDEX isuumo_price_id_idx ON isuumo.chair (price ASC, id ASC);
 CREATE INDEX estate_popularity_id_idx ON isuumo.estate (popularity DESC, id ASC);
+CREATE INDEX estate_rent_id_idx ON isuumo.estate (rent ASC, id ASC);
